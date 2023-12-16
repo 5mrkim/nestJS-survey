687ac83896beeb6c3d6f636ab7b2047b1f45f6d4
김선일 <gimseon-il@gimseon-il-ui-MacBookPro.local>
2023-12-16 13:57:36 +0900

    mod:테스트 푸시

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
 
   const config = new DocumentBuilder()
-    .setTitle('CICD 테스트입니다')
+    .setTitle('설문지API 문서')
     .setDescription('설문지 API 문서입니다!!')
     .setVersion('1.0.0')
     .addBearerAuth()
